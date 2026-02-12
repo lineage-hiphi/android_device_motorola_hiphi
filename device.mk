241ec670d223f41e5fdd3d3e6a6be8787ef5aced
Marc Bourgoin <themard85@gmail.com>
2026-02-11 22:40:07 -0700

    hiphi: Setup rootdir
    Change-Id: Iaa3cbf5345e375098888a1fcb72591b7504d74cf

--- device.mk
+++ device.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     init.mmi.overlay.rc \
-    init.vendor.st21nfc.rc
+    init.vendor.st21nfc.rc \
+    init.oem.fingerprint2.sh
 
 # NFC
 PRODUCT_PACKAGES += \
