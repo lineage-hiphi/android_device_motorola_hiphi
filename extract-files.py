af87bea71e6b2bc1135a0861dbd73cc6a4e1c689
Marc Bourgoin <themard85@gmail.com>
2026-02-24 07:41:25 -0700

    hiphi: Drop device firmware
    It seems that hiphic and hiphid cannot boot with hiphi device firmware,
    so unfortunately, we must drop it
    
    Change-Id: I31615bd2c42dd808ef7f1a3617ed02fc33a3bc68

--- extract-files.py
+++ extract-files.py
@@ -4,8 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.extract import extract_fns_user_type
-from extract_utils.extract_star import extract_star_firmware
 from extract_utils.fixups_blob import (
     BlobFixupCtx,
     File,
@@ -96,18 +94,12 @@ blob_fixups: blob_fixups_user_type = {
     ),
 }
 
-extract_fns: extract_fns_user_type = {
-    r'(bootloader|radio)\.img': extract_star_firmware,
-}
-
 module = ExtractUtilsModule(
     'hiphi',
     'motorola',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    extract_fns=extract_fns,
-    add_firmware_proprietary_file=True,
     add_generated_carriersettings=True,
 )
 
